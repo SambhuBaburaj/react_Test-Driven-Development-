49f1d4a4a88f71be2ef0f2b923355d282902558f
Sambhu Baburaj <sambhubaburah513@gmail.com>
2024-05-08 18:34:33 +0530

    react testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("codesweetly test heading", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole("heading")).toHaveTextContent(/codesweetly test/i);
 });
